91cf3c933cef81ea2038023d7a31188d8d5e0ff5
spencerhass <shass615@yahoo.com>
2015-09-21 09:11:05 -0400

    changed to create new game

--- js/app.js
+++ js/app.js
@@ -14,4 +14,53 @@ $(document).ready(function(){
 
 });
 
+/* creat new game */
+ $("new").click(function(event){
+ 		event.preventDefault();
+ 		newgame();
 
+ });
+
+ function newGame() {
+ 	guessFlag=true;
+ 	guessCount=0;
+ 	found= false;
+ 	$("ul#guessList li").remove();
+ 	setFeedback("Make your Guess!");
+ 	setCount(guessCount);
+ 	randomNumber=generateNumber();
+ 	setFocus();
+ 	clearText();
+
+ }
+
+ /*Random Number */
+
+ function generatedNumber() {
+ 	var generatedNumber = Math.floor((Math.random()*100)+1);
+	console.log("Generated Random Number = "+ generateNumber);
+
+	return generatedNumber;
+ }
+
+ /*focus input box */
+
+ function setFocus() {
+ 	document.getElementById("userGuess").focus();
+ }
+
+function clearText() {
+	$('#userGuess').val('');
+}
+
+/*Guess Count */
+
+function setCount (count) {
+	$('#count').text(guessCount);
+}
+
+function getChoice() {
+	var userChoice = prompt("Guess the Number", "Your Guess");
+	console.log ("User Choice = "+ userChoice);
+	return userChoice;
+}
