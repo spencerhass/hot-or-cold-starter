d304230ab2dd9e014e69e912396bd2f1a5dad4d7
spencerhass <shass615@yahoo.com>
2015-09-23 08:39:52 -0400

    added functions to file

--- js/app.js
+++ js/app.js
@@ -1,66 +1,103 @@
-
 $(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+  
+  /*--- Display information modal box ---*/
+    $(".what").click(function(){
+      $(".overlay").fadeIn(1000);
 
-  	});
+    });
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+    /*--- Hide information modal box ---*/
+    $("a.close").click(function(){
+      $(".overlay").fadeOut(1000);
+    });
 
+ var guessCount = 0;
+ var newGuess;
+ var randomNumber;
+ var guess;
+ var wonGame= false;
+
+ var generateNumber= function(){
+    randomNumber = Math.floor((Math.random() * 100) + 1);
+    console.log("randomNumber is" +randomNumber);
+ };
+
+ var guessCountDisplay = function(){
+  $("#count").text(guessCount);
+ };
+
+ var setFeedback = function(feedback) {
+  $("#feedback").text(feedback);
+ };
+
+ var clearGuess = function() {
+  $("#userGuess").val("").focus();
+ };
+
+ var removePastGuesses = function () {
+  $("ul.guessBox li").remove();
+ };
+
+ var checkGuess = function() {
+  guess = (Math.abs(randomNumber - newGuess));
+  if (guess === 0) {
+    setFeedback ("You guessed it");
+    $("#userGuess").val(randomNumber + "!");
+    wonGame=true;
+  } else if (guess<=5){
+    $("#feedback").text ("You're getting really hot");
+    return true;
+  } else if (guess <=10){
+    $("#feedback").text ("You're getting hot");
+    return true;
+  } else if (guess >=10 && guess <=20) {
+    $("#feedback").text ("You're getting warm");
+    return true;
+  } else if (guess>=20 && guess<=30) {
+    $("#feedback").text ("You're getting cold");
+    return true;
+  } else if (guess>=30 && guess <=40) {
+    $("#feedback").text ("You're getting very cold");
+    return true;
+  }else {
+    $("#feedback").text ("You're getting freezing");
+    return true;
+  }
+};
+
+
+  $("form").submit(function(event){
+  event.preventDefault();
+  if (wonGame === false) {
+    newGuess = +$("#userGuess").val();
+    /*--- Check if valid number --*/
+    if (newGuess % 1 !== 0 || newGuess > 100 || newGuess < 0) {
+      alert("Not a valid number");
+      return(false);
+    } else {
+      event.preventDefault();
+      $(".guessBox").append("<li>" +newGuess+ "</li>");
+      clearGuess();
+      guessCount++;
+      guessCountDisplay();
+      checkGuess();
+    }
+  } else {
+    AddFeedback("You already won! Start a new game.");
+  }
+
+  });
+
+
+$(".new").click(function(){
+    generateNewNumber(); 
+    clearGuess();
+    guessCount = 0;
+    wonGame = false;
+    removePastGuesses();
+    guessCountDisplay();
+    setFeedback("Make your guess!");
+  });
 });
 
-/* creat new game */
- $("new").click(function(event){
- 		event.preventDefault();
- 		newgame();
 
- });
-
- function newGame() {
- 	guessFlag=true;
- 	guessCount=0;
- 	found= false;
- 	$("ul#guessList li").remove();
- 	setFeedback("Make your Guess!");
- 	setCount(guessCount);
- 	randomNumber=generateNumber();
- 	setFocus();
- 	clearText();
-
- }
-
- /*Random Number */
-
- function generatedNumber() {
- 	var generatedNumber = Math.floor((Math.random()*100)+1);
-	console.log("Generated Random Number = "+ generateNumber);
-
-	return generatedNumber;
- }
-
- /*focus input box */
-
- function setFocus() {
- 	document.getElementById("userGuess").focus();
- }
-
-function clearText() {
-	$('#userGuess').val('');
-}
-
-/*Guess Count */
-
-function setCount (count) {
-	$('#count').text(guessCount);
-}
-
-function getChoice() {
-	var userChoice = prompt("Guess the Number", "Your Guess");
-	console.log ("User Choice = "+ userChoice);
-	return userChoice;
-}
